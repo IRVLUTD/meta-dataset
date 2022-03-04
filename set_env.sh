e19f29e1568af6efe16f7fa84cae1bd0bb956fd1
jishnjayakumar <jishnu.jayakumar182@gmail.com>
2022-03-04 15:19:42 -0600

    UPDATE dataset directory

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES="0";
 export BS=1;
 export ROOT_DIR=$PWD;
-export DATASRC="$ROOT_DIR/data";
+export DATASRC="$ROOT_DIR/datasets";
 export RECORDS="$ROOT_DIR/records";
 export SPLITS="$ROOT_DIR/meta_dataset/dataset_conversion";
 export EXPROOT=$ROOT_DIR/experiment_output;
