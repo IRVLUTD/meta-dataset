95f7744578e40213cef20f99692048dbd2fbbebe
jishnjayakumar <jishnu.jayakumar182@gmail.com>
2022-04-17 15:01:46 -0500

    FIX test all variants test script

--- __test.sh
+++ __test.sh
@@ -10,13 +10,21 @@ then
     _backbone="four_layer_convnet"
 fi
 
+num_valid_episodes=$5
+nve_suffix="-$num_valid_episodes"
+if test "$num_valid_episodes" = ""
+then
+    nve_suffix=""
+fi
+
+
 cd $RECORDS; rm tesla; ln -s $TESLA_DATASET_VARIANT tesla; cd $ROOT_DIR;
 
 ls -l $RECORDS # useful to check if sym links are correct
 
 for MODEL in $models
 do
-  export EXPNAME=${MODEL}_${SOURCE}
+  export EXPNAME=${MODEL}_${SOURCE}${nve_suffix}
   for DATASET in tesla
   do
     echo "MODEL-FILTER: $perform_filtration_model"
@@ -37,9 +45,8 @@ do
         --gin_bindings="Trainer.num_eval_episodes=600" \
         --gin_bindings="benchmark.eval_datasets='$DATASET'"
     else
-      model="${EXPNAME}${chkpt_suffix}${pretrained_phrase}-${backbone}"
-      # set BESTNUM to the "best_update_num" field in the corresponding best_....txt
-      export BESTNUM=$(grep best_update_num ${EXPROOT}/best_$model.txt | awk '{print $2;}')
+
+      export BESTNUM=$(grep best_update_num ${EXPROOT}/best_$EXPNAME.txt | awk '{print $2;}')
       python -m meta_dataset.train \
         --is_training=False \
         --records_root_dir=$RECORDS \
