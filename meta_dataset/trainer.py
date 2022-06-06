0b089ee2539fc7c20a6d999f34716d4617a7b299
jishnjayakumar <jishnu.jayakumar182@gmail.com>
2022-06-06 18:02:03 -0500

    FIX bug

--- meta_dataset/trainer.py
+++ meta_dataset/trainer.py
@@ -630,8 +630,8 @@ class Trainer(object):
     
     # get test classes
     total_classes = list(self.data_spec.classes_per_split.values())[2]
-    
-    _ = meta[str(total_classes)]
+    print(total_classes)
+    _ = meta[total_classes]
     prefix = \
       "clean-training" if "filtered" in self.checkpoint_to_restore.split("/")[-4] else "cluttered-training"
     
